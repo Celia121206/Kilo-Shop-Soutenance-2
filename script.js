a7ab1485f7fcd611557ba87f1c85367ef7ecb6b8
meysem1 <meysem.ess@gmail.com>
2025-06-09 18:26:20 +0200

    update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let isHidden = false
 window.addEventListener('scroll', function () {
     if (window.scrollY > 300 && !isHidden) {
         logo.style.opacity = '0'
-        logo.style.transition = 'opacity 0.2s ease-in-out'
+        logo.style.transition = 'opacity 0.3s ease-in-out'
         isHidden = true
         setTimeout(() => {
             if (isHidden) logo.style.display = 'none'
@@ -13,10 +13,30 @@ window.addEventListener('scroll', function () {
         logo.style.display = 'block'
         requestAnimationFrame(() => {
             logo.style.opacity = '1'
-            logo.style.transition = 'opacity 0.5s ease-in-out'
+            logo.style.transition = 'opacity 0.2s ease-in-out'
         })
         isHidden = false
     }
 })
 
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".card img");
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        }
+      });
+    }, {
+      threshold: 0.7 
+    });
+  
+    images.forEach((img) => {
+      observer.observe(img);
+    });
+  });
+  
 
